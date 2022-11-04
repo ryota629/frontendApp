4c271af13401922fbf16d5fa3a13eaff125a6155
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2022-11-04 21:11:56 +0900

    second commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,21 @@
-import { makeStyles } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  footer: {
-    color: "#ebf6f7",
-    backgroundColor: "#000000",
-    width: "100%",
-    position: "absolute",
-    bottom: "0px",
-    height: "20px",
-    verticalAlign: "middle",
-    textAlign: "center",
-  },
-});
+// const useStyles = makeStyles({
+//   footer: {
+//     color: "#ebf6f7",
+//     backgroundColor: "#000000",
+//     width: "100%",
+//     position: "absolute",
+//     bottom: "0px",
+//     height: "20px",
+//     verticalAlign: "middle",
+//     textAlign: "center",
+//   },
+// });
 
 function Footer() {
-  const classes = useStyles();
-  return <div className={classes.footer}>@2022</div>;
+  // const classes = useStyles();
+  return <div>@2022</div>;
 }
 
 export default Footer;
