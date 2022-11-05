781851d6dda202d820d59998ce3e0faf0a5c3b48
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2022-11-05 09:28:44 +0900

    server.js four ref

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "../movieapp-frontend", "build")));
+app.use(express.static('build'));
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "../movieapp-frontend", "build", "index.html"));
