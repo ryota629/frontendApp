c4873a08114074c03eb8f63eae7d17e8280e7a0b
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2022-11-05 09:14:10 +0900

    server.js refcter

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ const path = require("path");
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../movieapp-frontend', 'build')));
+app.use(express.static('build'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../movieapp-frontend', 'build'));
-});
-
-app.use('*',(req, res) => {
-  res.sendFile(path.join(__dirname, '../movieapp-frontend', 'build/index.html'));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "../movieapp-frontend", "build", "index.html"));
 });
 
 
